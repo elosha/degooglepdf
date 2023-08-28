722bcd80596f733c9e44dc6d7bc0a4037869c6f7
Eliza Winterborn <elosha@users.noreply.github.com>
2023-08-28 19:03:21 +0200

    Use sed for shell script and make it more reliable

--- mkpdf.sh
+++ mkpdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Prerequisites: base64, convert (ImageMagick)
+#Prerequisites: base64, sed, convert (ImageMagick)
 #Also I didn't managed to get the last page, but it's usually the back cover so I didn't lose my sleep for it, i'm sorry
 
 if [ ! -f "$1" ]; then
@@ -13,36 +13,24 @@ echo "Processing file: $1"
 
 mkdir -p "$1"_pages
 cd "$1"_pages
-PNGHEAD="data:image/png;base64,"
-COUNTER=0
+PNGHEAD="data:image\/png;base64,"
+COUNTER=1
 
-#echo "Extracting pages ..."
+echo -n "Extracting pages: "
 
 while read line
 do
 	if echo "$line" | grep -q $PNGHEAD; then
-		#echo "$COUNTER PNG header found."
-		if (( COUNTER )); then
-			#echo "$COUNTER Decoding Data."
-			echo "$data_img" | base64 -di - > $COUNTER.png
-			#echo "$COUNTER Data decoded."
-		fi
-		#echo "$COUNTER Removing header."
-		#FIXME: Hangs on Mac OS with 100% CPU
-		data_img="${line/$PNGHEAD/}"
-		#echo "$COUNTER Data set."
+		echo -n "$COUNTER, "
+		imgdata=$(echo "$line" | sed -e "s/^$PNGHEAD//")
+		echo "$imgdata" | base64 -di - > $COUNTER.png
 		let "COUNTER++"
-	else
-		#echo "$COUNTER One more line ..."
-		data_img+=$line$'\n'
 	fi
 done < ../"$1"
+echo "done."
 
-# Save last data
-echo "$data_img" | base64 -di - > $COUNTER.png
-
-echo "Converting to pdf ..."
-convert $(ls -1v) ../$1.pdf 
+echo -n "Converting to pdf ..."
+convert $(ls -1v *.png) ../$1.pdf 
 cd ..
 
-echo "PDF completed!"
+echo " completed!"
