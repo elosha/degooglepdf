af56b035788438a96adacdb16aca7ff31a7ee9f4
Eliza Winterborn <elosha@users.noreply.github.com>
2023-08-28 21:47:00 +0200

    Filenames and better file handling

--- mkpdf.sh
+++ mkpdf.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 #Prerequisites: base64, sed, convert (ImageMagick)
-#Also I didn't managed to get the last page, but it's usually the back cover so I didn't lose my sleep for it, i'm sorry
 
+if [ -z "$1" ]; then
+	echo "Usage: $0 input"
+	echo "Where \"input\" is the downloaded PDF page archive file name"
+	exit 1
+fi
 if [ ! -f "$1" ]; then
-	echo "Usage: $0 [basename]"
-	echo "Where \"basename\" is the downloaded PDF page archive's file name"
+	echo "Error: File $1 does not exist!"
 	exit 1
 fi
 
 echo "Processing file: $1"
 
-mkdir -p "$1"_pages
-cd "$1"_pages
+# Base file name without extension/spaces
+basename=${1/.imgpack/}
+basename=${basename/ /_}
+
+mkdir -p "$basename"_pages
+cd "$basename"_pages
 PNGHEAD="data:image\/png;base64,"
 JPEGHEAD="data:image\/jpeg;base64,"
 COUNTER=1
@@ -38,7 +45,7 @@ done < ../"$1"
 echo "done."
 
 echo -n "Converting to pdf ..."
-convert $(ls -1v *.jpg) ../$1.pdf
+convert $(ls -1v) "../$basename.pdf"
 cd ..
 
 echo " completed!"
